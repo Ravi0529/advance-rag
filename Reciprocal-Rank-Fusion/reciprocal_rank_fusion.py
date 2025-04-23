aaf8b5ab0141b8aef48e3b1dba622c9687d45eb0
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-23 20:07:49 +0530

    reciprocal rank fusion func added

--- Reciprocal-Rank-Fusion/reciprocal_rank_fusion.py
+++ Reciprocal-Rank-Fusion/reciprocal_rank_fusion.py
@@ -33,13 +33,33 @@ def generate_subqueries(user_query):
     return json.loads(response.choices[0].message.content)
 
 
-def reciprocal_rank_fusion():
-    pass
+def reciprocal_rank_fusion(
+    user_query,
+    collection_name="reciprocal_rank_fusion",
+    qdrant_url="http://localhost:6333",
+    k=60,
+):
+    sub_queries = generate_subqueries(user_query)
+    retriever = get_retriever(qdrant_url, collection_name)
+
+    scored_docs = {}
+    for sub_query in sub_queries:
+        results = retriever.similarity_search(sub_query, k=k)
+        for rank, doc in enumerate(results, start=1):
+            content = doc.page_content
+            score = 1 / (rank + 60)
+            scored_docs[content] = scored_docs.get(content, 0) + score
+
+    sorted_docs = sorted(scored_docs.items(), key=lambda x: x[1], reverse=True)
+    return [doc[0] for doc in sorted_docs]
 
 
 def get_final_answer(user_query, chunks):
     messages = [
-        {"role": "system", "content": "You are a helpful assistant..."},
+        {
+            "role": "system",
+            "content": "You are a helpful assistant that uses provided context to answer queries.",
+        },
         {
             "role": "user",
             "content": f"User asked: {user_query}\n\nHere is some information:\n\n{'\n\n'.join(chunks)}",
