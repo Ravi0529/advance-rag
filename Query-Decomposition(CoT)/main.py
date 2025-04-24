f9687f9920706af1f98b9338957d80d6bf529753
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-24 13:49:57 +0530

    query decomposition

--- Query-Decomposition(CoT)/main.py
+++ Query-Decomposition(CoT)/main.py
@@ -1,5 +1,20 @@
+from query_decomposition import query_decomposition, get_final_answer
+
+
 def main():
-    print("Hello from query!")
+    while True:
+        query = input("> ")
+        if query.lower() == "clear":
+            break
+
+        retrieved_data = query_decomposition(query)
+
+        if not retrieved_data:
+            print("No relevant results found")
+        else:
+            final_ans = get_final_answer(query, retrieved_data)
+
+            print(f"\nRelevant Result:\n{final_ans}")
 
 
 if __name__ == "__main__":
